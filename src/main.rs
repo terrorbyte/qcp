83e1e58c1159b8bf1659673b9cc736713740ee70
Ross Younger <crazyscot@gmail.com>
2024-11-02 20:55:33 +1300

    perf: use jemallocator on musl 64-bit builds

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 //! qcp utility - main entrypoint
 // (c) 2024 Ross Younger
 
+#[cfg(all(target_env = "musl", target_pointer_width = "64"))]
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 fn main() -> anyhow::Result<std::process::ExitCode> {
     qcp::cli()
 }
